36680c81c91ca3ddf42790f1a0176cccc34df527
Javier Urquizu <javier.urquizu@hp.com>
2016-12-27 09:49:55 -0800

    Added very bare skeleton for main.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "mbed.h"
+#include "initSensors.hpp"
+
+//#include <iostream> // Use pc instead of std?
+//#include "debug_io.hpp" // Uncomment if debugging
+
+int main()
+{
+	//std::cout << "testing" << std::endl;
+	return 0;
+}
