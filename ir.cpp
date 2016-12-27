ac46e1742a8fc0ba22cbe7ad0ce9bae2d4247e3d
Javier Urquizu <javier.urquizu@hp.com>
2016-12-27 09:48:46 -0800

    Finished IR sensor class.

--- ir.cpp
+++ ir.cpp
@@ -1,38 +1,55 @@
 #include "ir.hpp"
 
-/*
- * Constructor for IRController. Initializes IRPairs.
- */
-IRController::IRController()
-{
-	
-}
+// Initialize constant array COEFF, in the IRPair class (and all IRPair objects)
+// COEFF = [x^-3, x^-2, x^-1, x^0, x^1, x^2, x^3]
+const float IRPair::COEFF[] = {0.218, -5.510, 55.011, -229.934, 506.986, -502.862, 177.713};
 
 /*
- * Gets the distance to the wall in the direction specified.
- * Param: 
- *	dir - direction of the wall in query
+ * Constructor for IRPair. Initializes physical sensor pair.
+ */
+IRPair::IRPair(PinName rxPin, PinName txPin)
+	: IR_Receiver(rxPin) , IR_Emitter(txPin) { }
+
+/*
+ * Gets the distance to the wall.
  * Returns:
- *	dist - calculated distance in cm to the wall in direction dir
+ *	dist - calculated distance in cm to the wall
  */
-float IRController::dist_to_wall(char dir)
+float IRPair::dist_to_wall()
 {
-	// N,S,E,W are all relative, with N being the direction the mouse faces
-	switch(dir) {
-		case 'N': //TODO
-			break;
-		case 'W': //TODO
-			break;
-		case 'E': //TODO
-			break;
-		//DETERMINE IF EXTRA DIAGONAL CASES ARE NEEDED
-		default: //TODO handleError() or IRController::setFlag()
-			break;
+	float avgRead = 0;
+	
+	for (int i = 0; i < IR_SAMPLES; i++) {
+		// Turn on IR
+		this->IR_Emitter.write(1);
+		
+		// Wait for firing capacitor
+		wait_us(IR_SIGDELAY);
+		
+		// Read IR 
+		this->readLog[i] = this->IR_Emitter.read();
+		avgRead += this->readLog[i];
+		
+		// Turn off IR
+		this->IR_Emitter.write(0);
+		
+		// Recharge
+		wait_us(IR_SIGREST);
 	}
-	return 0.0;
-}
-
-int main()
-{
-	return 1;
+	avgRead /= IR_SAMPLES;
+	
+	// Use polynomial regression formula to generate distance to wall
+	float dist = 0;
+	float square = avgRead * avgRead;
+	float cube = square * avgRead;
+	
+	dist += this->COEFF[0] / cube;
+	dist += this->COEFF[1] / square;
+	dist += this->COEFF[2] / avgRead;
+	dist += this->COEFF[3];
+	dist += this->COEFF[4] * avgRead;
+	dist += this->COEFF[5] * square;
+	dist += this->COEFF[6] * cube;
+		
+	return dist;
 }
--- ir.hpp
+++ ir.hpp
@@ -1,45 +1,30 @@
 #ifndef IR_H
 #define IR_H
 
+#include "mbed.h"
 
-class IRController
-{
-	
+#define IR_SAMPLES 5
+#define IR_SIGDELAY 6 // Wait for firing capacitor signal (usec)
+#define IR_SIGREST 75 // Allows firing cap to recharge
+
+
+// Class for the IR Transmitter and Receiver Pair
+class IRPair
+{	
 public:
-	// Constants
+	// Constants	
 	static const float COEFF[]; // Coefficients found using linear regression (volts->cm)
+	float readLog[IR_SAMPLES]; // used for logging IR read values
 	
 	// Constructors
-	IRController();
+	IRPair(PinName rxPin, PinName txPin);
 	
 	// Methods
-	float dist_to_wall(char dir); // Returns dist in cm
+	float dist_to_wall(); // Returns dist in cm
 	
-	
-protected:
-	// Class for the IR Transmitter and Receiver Pair
-	class IRPair
-	{
-		public:
-			IRPair();
-			float readIR();
-			
-		private:
-			//DigitalOut	TXPin;
-			//AnalogIn 	RXPin;
-	};
-	
-	// Constants
-	static const short int SIGDELAY = 6; // Wait for firing capacitor signal (usec)
-	static const short int NSAMPLES = 5;
-	
-	// Data Members
-	//IRPair IRs[4]; // Order of IR Pairs: {FL, L, R, FR}
-	
+private:
+	AnalogIn 	IR_Receiver;
+	DigitalOut	IR_Emitter;
 };
-
-
-// COEFF = [x^-3, x^-2, x^-1, x^0, x^1, x^2, x^3]
-const float IRController::COEFF[] = {0.218, -5.510, 55.011, -229.934, 506.986, -502.862, 177.713};
 
 #endif 
