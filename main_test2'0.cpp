54b32ba4f5de3a75b82f769d03bccc29072cc95f
Javier Urquizu <javier.urquizu@hp.com>
2017-03-12 13:41:25 -0700

    Renamed and fixed encoder bug.

---
+++ main_test2'0.cpp
@@ -0,0 +1,25 @@
+#include "mbed.h"
+#include "./config/initDevices.h"
+//#include "maze.h"
+
+//#include <iostream> // Use pc instead of std?
+//#include "debug_io.hpp" // Uncomment if debugging
+
+int main()
+{
+	ledGreen = 1;
+	wait(0.5);
+	ledYellow = 1;
+	wait(0.5);
+	ledRed = 1;
+	wait(0.5);
+	
+	/*
+	leftMotor.go(1);
+	rightMotor.go(1);
+	wait(1);
+	leftMotor.stop();
+	rightMotor.stop();
+	*/
+	return 0;
+}
