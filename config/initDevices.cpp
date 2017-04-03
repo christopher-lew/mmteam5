6e73b670b389290541ca6dfbf21f4b3812b6a704
Javier Urquizu <javier.urquizu@hp.com>
2017-04-02 22:39:12 -0700

    Initiated Motor Objects.

--- config/initDevices.cpp
+++ config/initDevices.cpp
@@ -9,10 +9,10 @@
 	IRPair rightFrontIR(PC_4, PC_1);
 	IRPair rightIR(PA_6, PC_14);
 	IRPair testIR(PA_7, PH_0);
-	/*
+	
 	Motor leftMotor(PB_5, PB_4);
 	Motor rightMotor(PB_9, PB_8);
-
+	/*
 	Encoder leftEncoder(PB_10, PA_5);
 	Encoder rightEncoder(PA_0, PA_1); 
 	*/
@@ -39,10 +39,10 @@
 	IRPair leftFrontIR(PC_1, PA_7);
 	IRPair rightFrontIR(PC_0, PA_6);
 	IRPair rightIR(PH_0, PA_4);
-	/*
+
 	Motor leftMotor(PC_8, PC_7);
 	Motor rightMotor(PB_9, PB_8);
-
+	/*
 	Encoder leftEncoder(PB_3, PB_4);
 	Encoder rightEncoder(PB_7, PB_6); 
 	*/
