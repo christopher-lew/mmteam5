b7b70bf43eebf5fcfd55f2051ab00363230bc165
Javier Urquizu <javier.urquizu@hp.com>
2017-05-24 14:08:44 -0700

    Added zipper exe for Windows

---
+++ pyTools/PID Data Parser/pid_data_parser.py
@@ -0,0 +1,15 @@
+"""
+Created on 05/24/2017
+Python 3.5.3
+
+@author: Javier
+
+This script will parse pid data output by the Falcon.
+"""
+
+separatorString = "##### ##### #####"
+
+
+def read_trial_data():
+    pass
+
